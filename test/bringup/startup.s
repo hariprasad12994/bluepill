c8c509bc7e910cc8236cd37836430b4bd3342a27
Hari Prasad <hariprasad12994@gmail.com>
2022-05-29 22:46:02 +0530

    Adding proper sram init for bss and data segments

--- test/bringup/startup.s
+++ test/bringup/startup.s
@@ -4,6 +4,7 @@
 
 .word _data_start_
 .word _data_end_
+.word _data_start_flash_
 .word _bss_start_
 .word _bss_end_
 
@@ -23,5 +24,29 @@ isr_vectors:
   .type reset_handler, %function
 
 reset_handler:
+  ldr r0, =_bss_end_
+  ldr r1, =_bss_start_
+  cmp r0, r1
+  beq skip_bss_zeroing
+  mov r3, #0
+memset_bss_to_zero:    
+  str r3, [r1], #4
+  cmp r0, r1
+  bne memset_bss_to_zero
+skip_bss_zeroing:
+  ldr r0, =_data_end_
+  ldr r1, =_data_start_
+  cmp r0, r1
+  beq jump_to_main
+  ldr r2, =_data_start_flash_
+copy_data_segment:
+  ldr r3, [r2], #4
+  str r3, [r1], #4
+  cmp r0, r1
+  beq copy_data_segment
+jump_to_main:
   bl main
+halt:
+  b halt
+  .size reset_handler, .-reset_handler
 

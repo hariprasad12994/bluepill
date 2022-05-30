2f8c443b341e7405d6415faae19a6cdab517c6da
Hari Prasad <hariprasad12994@gmail.com>
2022-05-30 17:09:15 +0530

    Checking data section init by using global variables

--- test/bringup/blink.c
+++ test/bringup/blink.c
@@ -1,14 +1,15 @@
 
+unsigned int max_delay = 25000;
+volatile unsigned int i = 0;
+
 int main(void) {
   volatile unsigned int *rcc_apb2enr = (unsigned int*)(0x40021000 + 0x18);
   volatile unsigned int *gpioc_crh = (unsigned int*)(0x40011000 + 0x04);
   volatile unsigned int *gpioc_odr = (unsigned int*)(0x40011000 + 0x0c);
-  volatile unsigned int i = 0;
-
   *rcc_apb2enr |= 1 << 4;
   *gpioc_crh = (*gpioc_crh & ~(0xf << ((13 - 8) * 4))) | (6 << ((13 - 8) * 4));
   while(1) {
-    for(i = 0; i < 250000; i++);
+    for(i = 0; i < max_delay; i++);
     *gpioc_odr ^= 1 << 13;
   }
 

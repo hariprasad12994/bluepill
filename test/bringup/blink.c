ed764ed2ead229c4d3fe55469afdde7e0cca2ad4
Hari Prasad <hariprasad12994@gmail.com>
2022-05-30 23:42:59 +0530

    Hello world finally

--- test/bringup/blink.c
+++ test/bringup/blink.c
@@ -1,11 +1,11 @@
 
-unsigned int max_delay = 25000;
+const unsigned int max_delay = 250000;
 volatile unsigned int i = 0;
+volatile unsigned int *rcc_apb2enr = (unsigned int*)(0x40021000 + 0x18);
+volatile unsigned int *gpioc_crh = (unsigned int*)(0x40011000 + 0x04);
+volatile unsigned int *gpioc_odr = (unsigned int*)(0x40011000 + 0x0c);
 
 int main(void) {
-  volatile unsigned int *rcc_apb2enr = (unsigned int*)(0x40021000 + 0x18);
-  volatile unsigned int *gpioc_crh = (unsigned int*)(0x40011000 + 0x04);
-  volatile unsigned int *gpioc_odr = (unsigned int*)(0x40011000 + 0x0c);
   *rcc_apb2enr |= 1 << 4;
   *gpioc_crh = (*gpioc_crh & ~(0xf << ((13 - 8) * 4))) | (6 << ((13 - 8) * 4));
   while(1) {
